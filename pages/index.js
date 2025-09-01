c5dda44280edef8b4e4ab60b05f4b2a633b798eb
Don Fontijn <donfontijn@gmail.com>
2025-09-01 15:53:20 +0200

    Redesign hockey field to exactly match provided PNG reference
    - Change from square (aspect-[1/1]) to rectangular (aspect-[3/2]) field
    - Make center line thicker (h-2) and properly centered
    - Create large semicircles that span the full width of the field
    - Use border-4 for thicker, more visible semicircle borders
    - Remove all extra markings - only center line + 2 semicircles
    - Match the exact proportions and layout from the PNG image
    - Light gray background with thick black lines as shown in reference

--- pages/index.js
+++ pages/index.js
@@ -159,16 +159,22 @@ export default function RijnvlietHockeyApp() {
             
             {/* Hockey Field and Player Selection Layout */}
             <div className="flex gap-6">
-              {/* Hockey Field - Left side, narrower */}
+              {/* Hockey Field - Left side, matching PNG exactly */}
               <div className="flex-1">
                 <div className="relative bg-gray-300 rounded-lg p-4">
-                  <div className="aspect-[1/1] bg-gray-200 rounded border-4 border-white relative">
-                    {/* Center line - thick black line */}
-                    <div className="absolute top-1/2 left-0 right-0 h-1 bg-black"></div>
+                  <div className="aspect-[3/2] bg-gray-200 relative">
+                    {/* Center line - thick black line across the middle */}
+                    <div className="absolute top-1/2 left-0 right-0 h-2 bg-black transform -translate-y-1/2"></div>
                     
-                    {/* Goal areas - large semicircles touching the edges */}
-                    <div className="absolute top-0 left-0 right-0 h-1/2 border-2 border-black border-b-0 rounded-t-full"></div>
-                    <div className="absolute bottom-0 left-0 right-0 h-1/2 border-2 border-black border-t-0 rounded-b-full"></div>
+                    {/* Top goal area - large semicircle spanning full width */}
+                    <div className="absolute top-0 left-0 right-0 h-1/2">
+                      <div className="w-full h-full border-4 border-black border-b-0 rounded-t-full"></div>
+                    </div>
+                    
+                    {/* Bottom goal area - large semicircle spanning full width */}
+                    <div className="absolute bottom-0 left-0 right-0 h-1/2">
+                      <div className="w-full h-full border-4 border-black border-t-0 rounded-b-full"></div>
+                    </div>
                     
                     {/* Player positions - 8 players on field */}
                     {[1, 2, 3, 4, 5, 6, 7, 8].map((position) => {
