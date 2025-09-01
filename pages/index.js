36c6ac521f7e23ca699177d4f04e86b7dfee0955
Don Fontijn <donfontijn@gmail.com>
2025-09-01 10:30:00 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -550,7 +550,7 @@ export default function RijnvlietHockeyApp() {
       )}
     </div>
   );
-} Match Modal */}
+{/* Edit Match Modal */}
       {editingMatch && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
           <div className="bg-white rounded-lg p-6 w-full max-w-md">
